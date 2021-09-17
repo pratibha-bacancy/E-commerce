0b1d9e0aaac11ff3fe37f56cc6bf8b82daf847de
Pratibhapatil25 <pratibha.patil@bacancy.com>
2021-09-17 18:41:57 +0530

    updated files

--- E-commerce/src/main/java/com/bacancy/spboot/model/Categories.java
+++ E-commerce/src/main/java/com/bacancy/spboot/model/Categories.java
@@ -5,15 +5,11 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sun.istack.NotNull;
 
 @Entity
