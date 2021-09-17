000fe303a48e915c19b37ead3421b3f1219cac95
Pratibhapatil25 <pratibha.patil@bacancy.com>
2021-09-17 11:57:01 +0530

    updated files

--- E-commerce/src/main/java/com/bacancy/spboot/repository/ProductsRepository.java
+++ E-commerce/src/main/java/com/bacancy/spboot/repository/ProductsRepository.java
@@ -3,8 +3,8 @@ package com.bacancy.spboot.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.bacancy.spboot.model.Products;
+
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
 
